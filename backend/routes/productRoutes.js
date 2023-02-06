80b2b3f56bf2488364d0f4e522dd5eec4bfb1a7b
Mohit Jain <jain.mohit27@gmail.com>
2023-02-07 02:57:11 +0530

    Split router and Controller logic

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,11 @@
 import express from "express";
-import asyncHandler from "express-async-handler"
-import Product from "../models/productModel.js"
+import { getProducts, getProductById } from "../controllers/productController.js"
 
 const router = express.Router()
 
-router.get("/", asyncHandler( async (request, response) => {
-    const products = await Product.find({})
-    response.json(products)
-}))
+router.route("/").get(getProducts)
 
-router.get("/:id", asyncHandler( async (request, response) => {
-    const product = await Product.findById(request.params.id)
-    if(product){
-        response.json(product)
-    }else {
-        response.status(404)
-        throw new Error("Product not found.")
-    }
-    
-}))
+router.route("/:id").get(getProductById)
+
 
 export default router
