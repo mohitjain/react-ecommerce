9d8c4e73798e27493bfcf1a79eb3f3bd89f31fca
Mohit Jain <jain.mohit27@gmail.com>
2023-02-07 22:54:24 +0530

    Top Nav bar + Logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
@@ -14,8 +15,9 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route path="/product/:id" element={<ProductScreen/>}/>
-            <Route path="/" element={<HomeScreen/>}/>
+              <Route path="/product/:id" element={<ProductScreen/>}/>
+              <Route path="/login" element={<LoginScreen/>}/>
+              <Route path="/" element={<HomeScreen/>}/>
               <Route path="/cart/:id?" element={<CartScreen/>}/>
           </Routes>
         </Container>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,18 +3,22 @@ import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import { productListReducers, productDetailsReducers } from './reducers/productReducers'
 import { cartReducer } from "./reducers/cartReducers";
-
+import {userLoginReducers} from "./reducers/userReducers";
 
 const reducers = combineReducers({
     productList: productListReducers,
     productDetails: productDetailsReducers,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducers
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage }
+    cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage }
 }
 const middleware = [thunk]
 
